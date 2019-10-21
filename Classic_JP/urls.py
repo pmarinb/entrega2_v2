3459eb89def0707d366af38f394ddf99f8f0e3a1
Pablo Marin Bormuth <p.marinb@alumnos.duoc.cl>
2019-10-21 20:46:44 -0300

    agregado funcionalidad login

--- Classic_JP/urls.py
+++ Classic_JP/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('aplicaciones.usuario.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='usuario/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='usuario/login.html'), name='logout'),
 ]
