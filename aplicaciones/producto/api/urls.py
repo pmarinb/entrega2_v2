264497c9e2e6192f3b1d6dd54da7cac18cf286fb
Pablo Marin Bormuth <p.marinb@alumnos.duoc.cl>
2019-11-25 18:00:45 -0300

    Agregado permisos solo admin puede utilizar el crud, el resto solo podra listar. agregado login desde la ventana API

--- aplicaciones/producto/api/urls.py
+++ aplicaciones/producto/api/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
 from .views import VehiculoViewSet
+from django.urls import path, include
 
 router =  routers.SimpleRouter()
 router.register('vehiculo', VehiculoViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('', include('rest_framework.urls')),
+]
--- aplicaciones/producto/api/views.py
+++ aplicaciones/producto/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from aplicaciones.producto.models import Vehiculo
 from .serializer import VehiculoSerializer
+from .permissions import IsAdminUserOrReadOnly
 
 class VehiculoViewSet(viewsets.ModelViewSet):
     queryset = Vehiculo.objects.all()
     serializer_class = VehiculoSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
